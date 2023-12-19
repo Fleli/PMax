b6feef3299e10c4a5df6e67623c8425c2ec824dc
Fleli <frederikedvardsen@gmail.com>
2023-12-19 18:04:47 +0100

    Updated repository intialization.

--- Sources/PMax/_pmax/Init/MakefileDefault.swift
+++ Sources/PMax/_pmax/Init/MakefileDefault.swift
@@ -4,25 +4,44 @@ struct MakefileDefault {
     
     static func text(_ targetName: String, _ asLibrary: Bool) -> String {
         
-        let buildAsLibrary = asLibrary ? "--as-library " : ""
+        guard asLibrary else {
+            return initializeAsExecutable(targetName)
+        }
+        
+        return """
+        
+        all:
+            @echo "Initialization as library is not supported yet."
+        
+        """
+        
+    }
+    
+    
+    private static func initializeAsExecutable(_ targetName: String) -> String {
+        
         let libraryPaths = "_libraries"
         
         return """
         
-        ASLIBRARY = \(buildAsLibrary)
         LIBRARYPATHS = "\(libraryPaths)"
         ASMFILE = "out.bba"
         MACHINEFILE = "out.bbx"
-
+        PROFILECOMPILER = --profile
+        VIEWEXECUTION =
+        EMITINTERMEDIATE = #--emit-offsets --emit-tac --emit-pil
+        EMITINDICES = #--emit-indices
+        MAXINSTRUCTIONS = 10000000
+        
         all:
             @clear
             @echo "Compiling (pmax -> assembly)"
-            pmax build --target-name $(ASMFILE) $(ASLIBRARY)--lib-paths $(LIBRARYPATHS)
+            @pmax build --target-name $(ASMFILE) --lib-paths $(LIBRARYPATHS) $(PROFILECOMPILER) $(EMITINTERMEDIATE)
             @echo "Assembling (assembly -> binary)"
-            bbasm assemble _targets/$(ASMFILE) _targets/$(MACHINEFILE)
+            @bbasm assemble _targets/$(ASMFILE) _targets/$(MACHINEFILE) $(EMITINDICES) --print-stats
             @echo "Running (binary)"
-            bbvm _targets/$(MACHINEFILE)
-
+            @bbvm run _targets/$(MACHINEFILE) $(VIEWEXECUTION) --max-instructions $(MAXINSTRUCTIONS)
+        
         """
         
     }
