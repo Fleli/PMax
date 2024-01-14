a58317f4f06012f4e1f1f8b1f046d92f38aeaa28
Fleli <frederikedvardsen@gmail.com>
2024-01-14 13:55:01 +0100

    Commented PILScope.

--- Sources/PMax/Compiler/1PIL/_Core/_PILScope.swift
+++ Sources/PMax/Compiler/1PIL/_Core/_PILScope.swift
@@ -1,16 +1,21 @@
 class PILScope {
     
+    /// The parent of this scope. The parent scope is often referred to as the _outer scope_.
     let parent: PILScope?
     
+    /// A dicionary of the variables declared in this scope. Each name is mapped to its type.
     private(set) var variables: [String : PILType] = [:]
     
+    /// The `PILLowerer` object associated with this `PILScope` instance.
     private weak var lowerer: PILLowerer?
     
+    /// Initializes a new global scope. The scope is considered global because it won't have a parent scope (so no outer scope exists).
     init(_ lowerer: PILLowerer) {
         self.parent = nil
         self.lowerer = lowerer
     }
     
+    /// Initialize a scope from a parent scope. The child will inherit the parent's associated `PILLowerer` instance.
     init(_ parent: PILScope) {
         self.parent = parent
         self.lowerer = parent.lowerer
@@ -30,6 +35,9 @@ class PILScope {
         
     }
     
+    /// Returns the type of a variable `name` if it exists (returns `nil` otherwise).
+    /// Starts searching through this scope. If the variable is not found, it will (recursively) search the parent scope.
+    /// Thus, the search starts locally and moves "out" towards the global scope.
     func getVariable(_ name: String) -> PILType? {
         
         if let local = variables[name] {
