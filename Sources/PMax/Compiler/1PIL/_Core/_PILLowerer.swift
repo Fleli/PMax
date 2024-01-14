0b2686a0dd4f9fbf205fd2361925150000969711
Fleli <frederikedvardsen@gmail.com>
2024-01-14 11:50:01 +0100

    Added comments to PILLowerer and fixed issue where reserved identifiers such as 'int' could be used as struct/function names.

--- Sources/PMax/Compiler/1PIL/_Core/_PILLowerer.swift
+++ Sources/PMax/Compiler/1PIL/_Core/_PILLowerer.swift
@@ -3,35 +3,46 @@ class PILLowerer {
     /// The `local` scope is used to notify the `PILLowerer`'s environment of declarations, and to verify the existence of variables when they are referenced.
     var local: PILScope!
     
-    let id = Int.random(in: 1 ... 10000000)
-    
+    /// Returns `true` if no issues (i.e. errors that cannot be ignored) are found in PIL.
     var noIssues: Bool {
         return errors.reduce(true, {$0 && $1.allowed})
     }
     
+    /// Returns a readable description of the whole PIL environment. This includes all functions and structs declared.
     var readableDescription: String {
         functions.reduce("") { $0 + $1.value.fullDescription + "\n" }
         + structs.reduce("") { $0 + $1.value.description + "\n" }
     }
     
+    /// Returns a new anonymous variable (i.e. `%n` for an integer `n`). Its name is guaranteed to not collide with any other variables.
     var newAnonymousVariable: String {
         self.anonymousVariableCounter += 1
         return "%\(anonymousVariableCounter)"
     }
     
+    /// The number of anonymous variables introduced. Used to avoid name collisions.
     private var anonymousVariableCounter = 0
     
+    /// The preprocessor object responsible for handling imports.
     private let preprocessor: Preprocessor
+    
+    /// The `TopLevelStatements` object, which is an array of top-level statements (imports, functions and structs).
     private let topLevelStatements: TopLevelStatements
     
+    /// All errors (issues and warnings) found in the PIL stage.
     private(set) var errors: [PMaxError] = []
     
+    /// All structs declared, including those that are imported.
     private(set) var structs: [String : PILStruct] = [:]
+    
+    /// All functions declared, including those that are imported.
     private(set) var functions: [String : PILFunction] = [:]
     
+    /// A dictionary that maps a struct name to that struct's corresponding memory layout (`MemoryLayout`).
+    /// The memory layout describes the internal offsets for each member of the struct.
     private(set) var structLayouts: [String : MemoryLayout] = [:]
     
-    
+    /// Initialize a `PILLowerer` object from a number of top-level statements (the whole program) and a preprocessor that handles imports.
     init(_ topLevelStatements: TopLevelStatements, _ preprocessor: Preprocessor) {
         
         self.topLevelStatements = topLevelStatements
@@ -40,7 +51,10 @@ class PILLowerer {
         
     }
     
-    
+    /// Run PIL lowering.
+    /// This will populate the `structs` and `functions` dictionaries. It will also ask the preprocessor to run.
+    /// After `lower` is finished, `structs` and `functions` can be used for further lowering to TAC.
+    /// If the `noIssues` property on this `TACLowerer` is `false` after running `lower()`, an issue is found, so compilation should be aborted.
     func lower() {
         
         prepare()
@@ -53,21 +67,28 @@ class PILLowerer {
         
     }
     
+    /// Prepare the `PILLowerer` to lower each function body.
+    /// Preparation means _declaring_ all structs, which is essential to do so that functions anytwhere can use a struct type, regardless of whether it was declared before or after the struct.
+    /// Preparation also means importing libraries and adding structs & functions declared there to the relevant dictionaries, making them available to the code written by the user.
+    /// After imports are resolved and structs are declared, all functions are also declared. This is done afterwards because any function must be able to refer to any type, so they can't be declared until all structs are found.
     private func prepare() {
         
-        // First go through structs and imports
-        
+        // Go through each top-level statement.
         for syntacticStatement in topLevelStatements {
             
             switch syntacticStatement {
+                
+            // If the top-level statement is a struct, declare it.
             case .struct(let `struct`):
                 
                 self.newStruct(`struct`)
                 
+            // Ignore it (for now) if it is a function.
             case .function(_):
                 
                 continue
                 
+            // If the top-level statement is an import, ask the preprocessor to import it.
             case .import(let `import`):
                 
                 let library = `import`.library
@@ -77,12 +98,14 @@ class PILLowerer {
             
         }
         
-        // ... then lower functions to PIL
-        
+        // Now that every struct is declared, we can start defining functions.
+        // We had to wait since a function F might depend on a struct type T declared afterwards in the source code.
         for syntacticStatement in topLevelStatements {
             
+            // We ignore everything but functions.
             if case .function(let function) = syntacticStatement {
                 
+                // Build a new PILFunction object from the syntactical function.
                 let pilFunction = PILFunction(function, self)
                 self.newFunction(pilFunction)
                 
@@ -92,6 +115,9 @@ class PILLowerer {
         
     }
     
+    /// Lowering function bodies is the second step in lowering to PIL.
+    /// This is called after preparations are done (so we have _created_ all `PILFunction` objects, but their bodies haven't been lowered yet).
+    /// This function simply asks each function to lower itself to `PIL`. See the `lowerToPIL(_:)` method on `PILFunction` for more on how this is done.
     private func lowerFunctionBodies() {
         
         for function in functions.values {
@@ -100,29 +126,35 @@ class PILLowerer {
         
     }
     
+    /// We also need to find the memory layouts of each struct type.
+    /// A memory layout contains information on how the struct members are organized within the struct instance.
     private func findMemoryLayouts() {
+        
+        // We go through each struct type
         for `struct` in structs.values {
+            
+            // We generate a memory layout and pass `self` since structs need access to all other defined structs as well.
             let layout = `struct`.memoryLayout(self)
             structLayouts[`struct`.name] = layout
             
-            guard let layout else {
-                continue
-            }
-            
-            Compiler.print("Memory layout for '\(`struct`.name)': \(layout)")
-            
         }
+        
     }
     
+    /// Push a new scope on top of the current local scope.
+    /// Call this function when entering the body of statements such as `if`, `while`, etc.
     func push() {
         local = PILScope(local)
     }
     
+    /// Pop the current local scope.
+    /// Call this function when leaving the body of statements such as `if`, `while`, etc.
     func pop() {
         local = local.parent!
     }
     
-    /// Verify that `type` is a struct and that it has a field named `field`. Return the type if it exists. The `fieldType` method will submit any necessary error messages.
+    /// Return the type of the field `field` in the struct type `type`.
+    /// Will submit an error message if `type` is not a struct type, or if the field does not exist. In this case, `PILType.error` is returned.
     func fieldType(_ field: String, of type: PILType) -> PILType {
         
         if case .error = type {
@@ -148,42 +180,60 @@ class PILLowerer {
         
     }
     
+    /// Find the return type of a function whose name is `functionName`.
+    /// Returns `PILType.error` if no such function exists.
     func functionType(_ functionName: String) -> PILType {
         return functions[functionName]?.type ?? .error
     }
     
+    /// Submit an error.
     func submitError(_ newError: PMaxError) {
         errors.append(newError)
     }
     
-    
-    /// Take a syntactical `Struct` object and add it to the internal dictionary of structs.
+    /// Take a syntactical `Struct` object, verify that it does not name clash with any existing types,
     func newStruct(_ `struct`: Struct) {
         
+        // The struct's name is now considered a new type.
         let type = `struct`.name
         
-        guard structs[type] == nil else {
+        // Check that no struct with the same name already exists.
+        guard nameIsAvailable(type) else {
             submitError(PMaxIssue.invalidRedeclaratino(struct: type))
             return
         }
         
+        // Create a new `PILStruct` object, and add it to the structs dictionary.
         let newStruct = PILStruct(`struct`, self)
         structs[type] = newStruct
         
     }
-    
     
     /// Take a `PILFunction` object and add it to the internal dictionary of functions.
     func newFunction(_ pilFunction: PILFunction) {
         
         let name = pilFunction.name
         
-        guard functions[name] == nil else {
+        guard nameIsAvailable(name) else {
             submitError(PMaxIssue.invalidRedeclaration(function: name))
             return
         }
         
         functions[name] = pilFunction
+        
+    }
+    
+    /// Returns `true` if a  name is available (i.e. no type/function uses it), and `false` otherwise.
+    func nameIsAvailable(_ name: String) -> Bool {
+        
+        // TODO: Make sure there is consistency between Builtin and this list.
+        // This should be solved better than simply ORing different cases. Look into this.
+        
+        if name == Builtin.void || name == Builtin.native || name == Builtin.char {
+            return false
+        }
+        
+        return functions[name] == nil && structs[name] == nil
         
     }
     
--- Sources/PMax/_pmax/Version/Version.swift
+++ Sources/PMax/_pmax/Version/Version.swift
@@ -6,8 +6,8 @@ struct Version: ParsableCommand {
     
     func run() throws {
         
-        let date = "2024-01-11"
-        let version = "0.6.1"
+        let date = "2024-01-13"
+        let version = "0.6.2"
         
         print("Version \(version)\n\(date)")
         
